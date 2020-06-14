d1b4ea87c22741486f5b2dd435f5c22501ae8993
Thomas Roller <thomasroller@yahoo.ca>
2020-06-14 18:10:01 -0400

    Allow custom location and dimensions of game via command line

--- point_based/main.cpp
+++ point_based/main.cpp
@@ -4,10 +4,33 @@ File: main.cpp
 
 #include "Control.h"
 #include "FileIO.h"
+#include "constants.h"
+
+#include <exception>
 
 int main(int argc, char* argv[]) {
-    if (argc == 2) {
-        return Control::start(argv[1], 0, 0, 50, 50);
+    if (argc == 1) {
+        return Control::start("data.txt", 0, 0, 25, 25);
     }
-    return Control::start("data.txt", 0, 0, 50, 50);
+    else if (argc == 2) {
+        return Control::start(argv[1], 0, 0, 25, 25);
+    }
+
+    try {
+        if (argc == 4) {
+            return Control::start(argv[1], 0, 0, std::stoi(argv[2]), std::stoi(argv[3]));
+        }
+        else if (argc == 6) {
+            return Control::start(argv[1], std::stoi(argv[2]), std::stoi(argv[3]), std::stoi(argv[4]), std::stoi(argv[5]));
+        }
+    }
+    catch (std::exception e) {
+        if (SHOW_CRITICAL_MESSAGES) { std::cout << "main: INVALID ARGUMENT TYPE(S)" << std::endl; }
+    }
+
+    std::cout << "Usage: ./main" << std::endl;
+    std::cout << "       ./main <filename>" << std::endl;
+    std::cout << "       ./main <filename> <height> <width>" << std::endl;
+    std::cout << "       ./main <filename> <top row> <top column> <height> <width>" << std::endl;
+    return 0;
 }
