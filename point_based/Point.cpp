6b23fc485be06f00f876a6bcd0556335f4b2a3bd
Thomas Roller <thomasroller@yahoo.ca>
2020-07-14 20:22:47 -0400

    Begin per-function documentation

--- point_based/Point.cpp
+++ point_based/Point.cpp
@@ -4,6 +4,7 @@ File: Point.cpp
 
 #include "Point.h"
 
+// Operator overloading (important for ordering)
 bool Point::operator== (const Point& point) const {
     return x == point.getX() && y == point.getY();
 }
@@ -16,24 +17,61 @@ bool Point::operator> (const Point& point) const {
     return hash() > point.hash();
 }
 
+/* Function: Point
+ * Purpose: default constructor
+ * Arguments:
+ *     none
+ * Return
+ *     none
+ */
 Point::Point () {
     init(0, 0);
 }
 
+/* Function: Point
+ * Purpose: constructor which sets x and y variables
+ * Arguments:
+ *     x (int): x coordinate
+ *     y (int): y coordinate
+ * Return
+ *     none
+ */
 Point::Point (int x, int y) {
     init(x, y);
 }
 
+/* Function: Point
+ * Purpose: copy constructor
+ * Arguments:
+ *     none
+ * Return
+ *     none
+ */
 Point::Point (const Point& point) {
     if (SHOW_DEBUG_MESSAGES) { std::cout << "Point (copy): copying Point" << this->toString() << std::endl; }
     x = point.x;
     y = point.y;
 }
 
+/* Function: ~Point
+ * Purpose: destructor
+ * Arguments:
+ *     none
+ * Return
+ *     none
+ */
 Point::~Point () {
     if (SHOW_DEBUG_MESSAGES) { std::cout << "~Point: destroying Point " << toString() << std::endl; }
 }
 
+/* Function: init
+ * Purpose: initializes coordinates
+ * Arguments:
+ *     x (int): x coordinate
+ *     y (int): y coordinate
+ * Return
+ *     (void)
+ */
 void Point::init (int x, int y) {
     instances = 0;
     if (abs(y) >= (POINT_HASH_MODIFIER / 10)) {
@@ -45,21 +83,44 @@ void Point::init (int x, int y) {
     Point::y = y;
 }
 
+// Getters
 int Point::getX () const { return x; }
 int Point::getY () const { return y; }
 
+/* Function: hash
+ * Purpose: get a unique numeric value for a point
+ * Arguments:
+ *     none
+ * Return
+ *     (int) unique numeric value
+ */
 int Point::hash () const {
     return (POINT_HASH_MODIFIER * x) + y;
 }
 
+/* Function: toString
+ * Purpose: provide a string representation of the Point
+ * Arguments:
+ *     none
+ * Return
+ *     (string) string representation of the Point
+ */
 std::string Point::toString () const {
     return "(" + std::to_string(x) + ", " + std::to_string(y) + ")";
 }
 
+/* Function: copy
+ * Purpose: create a new Point object
+ * Arguments:
+ *     none
+ * Return
+ *     (Point) copy of the current Point
+ */
 Point Point::copy () {
     return Point(x, y);
 }
 
+// Functions to manage the number if instances of the Point
 void Point::addUse () { ++instances; }
 void Point::removeUse () { if (instances > 0) { --instances; } }
 int Point::getUses () { return instances; }
